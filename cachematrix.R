ec2b8417890649135a6b2c4108a402d12aefb13c
Michal Krason <michal.krason1@gmail.com>
2014-12-20 14:09:18 +0100

    Updating the file to contain the complete assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Below you can find implementations of two functions that enable caching the result of matrix inversion
+## First of them constructs the object which enables the caching and the second is the interface to use the object
 
-## Write a short comment describing this function
+## This function creates a 4 element object that enables storing and viewing a matrix and its inversion 
+## (this function doesn't the second object to be an inversion, however we will use it for this purpose)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv<-NULL
+  set <- function(y) {
+    x<<-y
+    inv<<-NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) inv<<-inverse
+  getinv <- function() inv
+  return(list(set=set, get=get, setinv=setinv, getinv=getinv ))
 }
 
 
-## Write a short comment describing this function
+## This function checks whether matrix inversion is already calculated for the object.
+## If yes, it returns the existing result, else it calculates the inversion, stores and returns it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv<-x$getinv()
+  if(!is.null(inv)) {
+    message("Data found in cache, returning")
+    return(inv)
+  }
+  inv<-solve(x$get(),...)
+  x$setinv(inv)
+  return(inv)
 }
